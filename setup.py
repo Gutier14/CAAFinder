3118ef309b46204bc03c99e6be843fc65bc1fb99
Gutier14 <305700384@qq.com>
2016-06-05 01:10:58 +0800

    add a release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'caafinder',
-    version = '0.1',
+    version = '0.1.1',
     keywords='ds caa',
     description = 'a library for DS CAA Developer',
     license = 'MIT License',
