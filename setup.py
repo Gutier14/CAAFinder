ad0a9b58b927543acb660dbff57b447ec5d520e5
Gutier14 <305700384@qq.com>
2016-06-05 00:46:06 +0800

    build the project Env.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'caafinder',
+    version = '0.1',
+    keywords='ds caa',
+    description = 'a library for DS CAA Developer',
+    license = 'MIT License',
+    url = 'https://github.com/Gutier14/CAAFinder',
+    author = 'Luca Liu',
+    author_email = 'geekluca@qq.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [],
+)
