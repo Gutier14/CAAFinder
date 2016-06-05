cdc2f38133aa02e86f709daa715a5f51e587a6f1
Gutier14 <305700384@qq.com>
2016-06-05 17:31:21 +0800

    add version 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'caafinder',
-    version = '0.1.2',
+    version = '0.1.3',
     keywords='ds caa',
     description = 'a library for DS CAA Developer',
     license = 'MIT License',
